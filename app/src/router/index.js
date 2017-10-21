a35b8951e533550dc413a2d5b6a97413b7246318
occammedia <carl.p@freshly-squeezed.com>
2017-10-21 13:02:36 +0100

    Initial setup of Vue app

---
+++ app/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Form from '@/components/Form'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+
+    {
+      path: '/Form',
+      name: 'Form',
+      component: Form
+    }
+
+  ]
+})
